11859543facfe83ad90ab5a27135346f7a054a7b
Julia Mackamul <julia.mackamul@gmail.com>
2020-07-03 18:15:09 +0200

    active nav

--- JS/nav.js
+++ JS/nav.js
@@ -23,3 +23,16 @@
     });
   }); // end DOM ready
 })(jQuery); // end jQuery
+
+// ACTIVE NAV LINK
+
+(function () {
+  var current = location.pathname.split("/")[1];
+  if (current === "") return;
+  var menuItems = document.querySelectorAll(".menu-item a");
+  for (var i = 0, len = menuItems.length; i < len; i++) {
+    if (menuItems[i].getAttribute("href").indexOf(current) !== -1) {
+      menuItems[i].className = "is-active";
+    }
+  }
+})();
