1aa4bd387ec78fedfc6f8d6c9ad6a3ee3a23e1c1
Julia Mackamul <julia.mackamul@gmail.com>
2020-07-05 22:02:35 +0200

    images quadratisch

--- JS/nav.js
+++ JS/nav.js
@@ -1,19 +1,3 @@
-// ACTIVE NAV LINK
-
-jQuery(document).ready(function ($) {
-  // Get current path and find target link
-  var path = window.location.pathname.split("/").pop();
-
-  // Account for home page with empty path
-  if (path == "") {
-    path = "index.html";
-  }
-
-  var target = $('nav a[href="' + path + '"]');
-  // Add active class to target link
-  target.addClass("active");
-});
-
 // DROPDOWN NAV
 
 (function ($) {
@@ -41,3 +25,19 @@ jQuery(document).ready(function ($) {
     });
   }); // end DOM ready
 })(jQuery); // end jQuery
+
+// ACTIVE NAV LINK
+
+jQuery(document).ready(function ($) {
+  // Get current path and find target link
+  var path = window.location.pathname.split("/").pop();
+
+  // Account for home page with empty path
+  if (path == "") {
+    path = "index.html";
+  }
+
+  var target = $('nav a[href="' + path + '"]');
+  // Add active class to target link
+  target.addClass("active");
+});
