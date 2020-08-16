2a62cb8b624944445570c397bdeff93144af8839
Julia Mackamul <julia.mackamul@gmail.com>
2020-08-16 10:22:38 +0200

    active nav

--- JS/nav.js
+++ JS/nav.js
@@ -28,16 +28,8 @@
 
 // ACTIVE NAV LINK
 
-jQuery(document).ready(function ($) {
-  // Get current path and find target link
-  var path = window.location.pathname.split("/").pop();
-
-  // Account for home page with empty path
-  if (path == "") {
-    path = "index.html";
-  }
-
-  var target = $('nav a[href="' + path + '"]');
-  // Add active class to target link
-  target.addClass("active");
+$(function () {
+  $('nav a[href^="/' + location.pathname.split("/")[1] + '"]').addClass(
+    "active"
+  );
 });
