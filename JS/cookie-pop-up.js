3a82b3707813e6979ef91f36b139ec7bd7858569
juliamackamul <julia.mackamul@gmail.com>
2022-10-27 11:13:22 +0200

    links footer

---
+++ JS/cookie-pop-up.js
@@ -0,0 +1,20 @@
+document.write(`
+<div id="cookie-popup" class="hidden">
+      <div class="cookies-container">
+        <p class="cookies-copy">
+          We use cookies to ensure you get the best experience on our website.
+        </p>
+
+        <div class="cookies-buttons">
+          <button class="cookies-btn-1" id="accept">
+            <b>OKAY, COOL</b>
+          </button>
+          <button class="cookies-btn-2">
+            <a href="./cookies.html" target="_blank" class="cookies-link"
+              >learn more</a
+            >
+          </button>
+        </div>
+      </div>
+    </div>
+`);
