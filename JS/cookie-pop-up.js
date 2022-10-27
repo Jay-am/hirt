1db2b6aa0a600f8fb1e6bca746f0413cd073375b
juliamackamul <julia.mackamul@gmail.com>
2022-10-27 12:44:04 +0200

    alignment impressum

--- JS/cookie-pop-up.js
+++ JS/cookie-pop-up.js
@@ -1,20 +1,17 @@
-document.write(`
-<div id="cookie-popup" class="hidden">
-      <div class="cookies-container">
-        <p class="cookies-copy">
-          We use cookies to ensure you get the best experience on our website.
-        </p>
+$("#accept").on('click',function(){
+  localStorage.setItem("cookieSaved","yes");
+  $("#cookie-popup").addClass("hidden");
+})
 
-        <div class="cookies-buttons">
-          <button class="cookies-btn-1" id="accept">
-            <b>OKAY, COOL</b>
-          </button>
-          <button class="cookies-btn-2">
-            <a href="./cookies.html" target="_blank" class="cookies-link"
-              >learn more</a
-            >
-          </button>
-        </div>
-      </div>
-    </div>
-`);
+window.onload = () =>
+{
+  var getCookie = localStorage.getItem("cookieSaved");
+  if(getCookie == "yes")
+  {
+      $("#cookie-popup").addClass("hidden");
+  }
+  else
+  {
+      $("#cookie-popup").removeClass("hidden");
+  }
+}
